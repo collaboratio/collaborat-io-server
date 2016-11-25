b1f48c07b882a0fb90f35fb9b63c93d171c2c2e6
Fernando Jiménez <the.merck@gmail.com>
2016-11-25 03:09:36 +0100

    Adapter modification and ECMAScript2015 modifications

--- index.js
+++ index.js
@@ -2,58 +2,55 @@
 
 var autoload = require('auto-load');
 
-module.exports = Collaboratio;
-
-/**
- * Collaboratio server constructor
- * @param {Socket.io} socket 	Socket.io socket
- * @param {http} 			server 	Http server
- * @param {String} 		port 		Port server
- */
-function Collaboratio (socket, server, port) {
-	if (!(this instanceof Collaboratio)) return new Collaboratio(socket, server, port);
-
-	this.socketio = socket;
-	this.server = server;
-	this.port = port || '2013';
-	this.managers = [];
-}
-
-/**
- * Load the events managers from /lib/managers folder
- */
-Collaboratio.prototype.loadEventManagers = function () {
-	var requires = autoload(__dirname + '/lib/managers');
-	for (var managerName in requires) {
-		if (requires.hasOwnProperty(managerName)) {
-			this.managers.push(requires[managerName]());
-		}
+class Collaboratio {
+	/**
+	* Collaboratio server constructor
+	* @param {Socket.io} socket 	Socket.io socket
+	* @param {http} 			server 	Http server
+	* @param {String} 		port 		Port server
+	*/
+	constructor(socket, server, port) {
+		this.socketio = socket;
+		this.server = server;
+		this.port = port || '2013';
+		this.managers = [];
 	}
 
-};
+	/**
+	 * Load the events managers from /lib/managers folder
+	 */
+	loadEventManagers (socket) {
+		var requires = autoload(__dirname + '/lib/managers');
+		for (var managerName in requires) {
+			if (requires.hasOwnProperty(managerName)) {
+				this.managers.push(new (requires[managerName])(socket));
+			}
+		}
 
-/**
- * Register events for each manager
- * @param  Socket.io socket
- */
-Collaboratio.prototype.registerEvents = function (socket) {
-	this.managers.forEach(function (manager) {
-		manager.registerEvents(socket);
-	})
-};
+	};
 
-/**
- * Initialize the Collaboratio Server
- */
-Collaboratio.prototype.init = function () {
-	this.server.listen(this.port);
+	/**
+	 * Register events for each manager
+	 * @param  Socket.io socket
+	 */
+	registerEvents ( ) {
+		this.managers.forEach( (manager) => { manager.registerEvents() });
+	};
 
-	// Execute adapter
-	var cioAdapter = require('./lib/adapter')(this.socketio);
-	cioAdapter.init();
+	/**
+	 * Initialize the Collaboratio Server
+	 */
+	init ( ) {
+		this.server.listen(this.port);
 
-	this.socketio.on('connection', function (socket) {
-		this.loadEventManagers();			// Loading event managers
-		this.registerEvents(socket);	// Register all events from managers
-	}.bind(this));
-};
+		// Execute adapter
+		var cioAdapter = require('./lib/adapter')(this.socketio);
+		cioAdapter.init();
+		this.socketio.on('connection', (socket) => {
+			this.loadEventManagers(socket);			// Loading event managers
+			this.registerEvents();	// Register all events from managers
+		});
+	};
+}
+
+module.exports = Collaboratio;
